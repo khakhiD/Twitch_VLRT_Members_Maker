be02a578724bec17656a5aaf72a64aeb8838772e
khakhiD <kakkiid@gmail.com>
2023-05-31 00:07:20 +0900

    style: card example

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,6 +3,10 @@ import twitchLogo from "/images/logo/logo-twitch.svg";
 import "./App.css";
 
 function App() {
+  const todayDate = () => {
+    return new Date().toLocaleDateString('ko-kr');
+  }
+
   return (
     <>
       <div>
@@ -13,7 +17,14 @@ function App() {
         <h1 id="text1" className="gradientText">트위치 발로란트 내전 카드 생성기</h1>
       </div>
       <div className="card">
-        <p>카드 생성 버튼을 눌러 새 카드를 만들 수 있습니다.</p>
+        <div className="cardExampleBg">
+            <h2>오늘의 발로란트 저티어 내전</h2>
+            <span>{todayDate()}</span>
+            <div className="col">
+              EXAMPLE
+            </div>
+        </div>
+        <p>카드 생성 버튼을 눌러 위와 같은 카드를 만들 수 있습니다.</p>
         <button>카드 생성하기</button>
       </div>
       <p className="read-the-docs">
